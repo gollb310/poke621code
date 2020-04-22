e3b9f85929a31c3a80dd4600ccfce596453137ef
gollb310 <bkg7290@g.rit.edu>
2020-04-22 16:21:35 -0400

    clear token & basic functionality

--- app/main.rb
+++ app/main.rb
@@ -1,10 +1,19 @@
 class Main
   require 'discordrb'
 
-  bot = Discordrb::Bot.new token: 'NzAyNTk2MzU1MDY2OTUzODQ4.XqCaEg.sQuVzP1W7NUDv9taO8U5PKWQixY', client_id: 168123456789123456
-
-  bot.message(with_text: 'Ping!') do |event|
-    event.respond 'Pong!'
+  bot = Discordrb::Bot.new token: '<token id>', client_id: 702596355066953848
+  targetChannelId = 0
+  bot.message(from: "Pokécord") do |event|
+    content = event.content
+    words = content.split
+    if(words[0] == "Congratulations")
+      pokelength = words[7].length - 2
+      pokemon = words[7][0..pokelength]
+      command = "b!e621 " + pokemon
+      bot.channel(targetChannelId).send_message(command)
+    end
   end
   bot.run
 end
+
+Main.new
