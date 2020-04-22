9f1d5ba776d5867683b8eb5080d97bb977e6712f
gollb310 <bkg7290@g.rit.edu>
2020-04-22 15:25:41 -0400

    test token stuff

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,7 @@
 class Main
   require 'discordrb'
 
-  bot = Discordrb::Bot.new token: 'NzAyNTk2MzU1MDY2OTUzODQ4.XqCZ0w.B--OaX0oKrSYAMz6Sjm6YZqWYqQ', client_id: 168123456789123456
+  bot = Discordrb::Bot.new token: 'NzAyNTk2MzU1MDY2OTUzODQ4.XqCaEg.sQuVzP1W7NUDv9taO8U5PKWQixY', client_id: 168123456789123456
 
   bot.message(with_text: 'Ping!') do |event|
     event.respond 'Pong!'
